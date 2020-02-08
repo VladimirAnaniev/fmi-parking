f81116c78fc9ce1aa0150bd5be1c0026c0a12310
Vladimir Ananiev <vladeto97@gmail.com>
2020-02-08 17:39:37 +0200

    List parking spots

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
-<?php
-include_once 'views/header.php';
+<?php include_once 'views/header.php'; ?>
+
+<?php 
+    require_once 'controllers/ParkingSpotsController.php';
+
+    $parking_spots_controller = new ParkingSpotsController();
+    $parking_spots = $parking_spots_controller->get_all_parking_spots();
+    $free_spots = $parking_spots_controller->count_free_spots($parking_spots)
 ?>
 
-<!-- TODO: list some data here so it is not an empty page.
-     For example a greeting, explanation and a prompt to login if user is not logged in
-     or display the free places visually if user is logged in -->
+<div class="container">
+    <?php if (is_logged_in()) { ?>
+            <h1>Статус на паркинга</h1>
+            <div class="card-deck">
+                <?php foreach ($parking_spots as $spot): ?>
+                <div class="card">
+                    <div class="card-body text-white <?php echo $spot->isFree() ? 'bg-success' : 'bg-danger' ?>">
+                        <div class="card-title">Място номер <?php echo $spot->getNumber() ?></div>
+                        <p class="card-text">
+                            <?php if($spot->isFree()) { ?>
+                                Мястото е свободно
+                            <?php } else { ?>
+                                <p>Заето от <?php echo $spot->getOwner() ?> с номер <?php echo $spot->getCar() ?>. </p>
+                                <p>Oт <?php echo $spot->getTimeIn() ?> до <?php echo $spot->getTimeOut() ?>.</p>
+                            <?php } ?>
+                        </p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+    <?php } else { ?>
+            <h1>Добре дошли!</h1> 
+            <h3>В момента има <?php echo $free_spots ?> свободни места.</h3>
+            <h3>За да видите повече детайли или да вдигнете бариерата, моля влезте в профила си.</h3>
+    <?php } ?>
+</div>
 
-<?php
-include_once 'views/footer.php';
-?>
+<?php include_once 'views/footer.php'; ?>
--- models/ParkingSpot.php
+++ models/ParkingSpot.php
@@ -3,5 +3,68 @@
 
 class ParkingSpot
 {
+    private  static $SELECT_ALL_PARKING_SPOTS = "SELECT * FROM parking_spots";
 
+    private $number;
+    private $car;
+    private $owner;
+    private $time_in;
+    private $duration;
+    private $time_out;
+    private $free;
+
+    public function __construct($number, $car, $owner, $time_in, $duration, $time_out, $free)
+    {
+        $this->number = $number;
+        $this->car = $car;
+        $this->owner = $owner;
+        $this->time_in = $time_in;
+        $this->duration = $duration;
+        $this->time_out = $time_out;
+        $this->free = $free;
+    }
+
+    public static function get_all($connection) {
+        $query = $connection->query(self::$SELECT_ALL_PARKING_SPOTS);
+
+        $parking_spots = array();
+        while ($row = $query->fetch()) {
+            $spot = new ParkingSpot($row["number"], $row["car"], $row["owner"], $row["time_in"],
+            $row["duration"], $row["time_out"], $row["free"]);
+
+            array_push($parking_spots, $spot);
+        }
+
+        return $parking_spots;
+    }
+
+    public function getNumber() {
+        return $this->number;
+    }
+
+    public function getCar() {
+        return $this->car;
+    }
+
+    public function getOwner() {
+        return $this->owner;
+    }
+
+    public function getTimeIn() {
+        return $this->time_in;
+    }
+
+    public function getDuration() {
+        return $this->duration;
+    }
+
+    public function getTimeOut() {
+        return $this->time_out;
+    }
+
+    public function isFree() {
+        return $this->free;
+    }
 }
+
+?>
