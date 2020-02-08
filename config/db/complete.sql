06f52109279cdde040bb607ff7d74dc825362c76
Hakan Halil <hhalil@hhalil-a01.vmware.com>
2020-02-08 16:03:58 +0200

    Address database config file issues

--- config/db/complete.sql
+++ config/db/complete.sql
@@ -92,28 +92,6 @@ INSERT INTO `parking_spots` (`number`, `car`, `owner`, `time_in`, `duration`, `t
 -- --------------------------------------------------------
 
 --
--- Table structure for table `timetable`
---
-
-CREATE TABLE `timetable` (
-  `subject` varchar(65) NOT NULL,
-  `room` int(11) NOT NULL,
-  `day` enum('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') NOT NULL,
-  `start_time` time NOT NULL,
-  `end_time` time NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `timetable`
---
-
-INSERT INTO `timetable` (`subject`, `room`, `day`, `start_time`, `end_time`, `user_id`) VALUES
-('DAA', 101, 'Monday', '12:00:00', '14:00:00', 31);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -132,7 +110,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_first`, `u_last`, `u_email`, `u_password`, `u_role`, `u_id`, `car`) VALUES
-('Александър', 'Гълъбов', 'admin@abv.bg', '$2y$10$36PFZVtgrbPgrznEhcA.M.Kn9clEk966F6fkh4hp9a42flHJApo1m', 'admin', 1, NULL),
+('Админисандър', 'Щъркелов', 'admin@abv.bg', '$2y$10$36PFZVtgrbPgrznEhcA.M.Kn9clEk966F6fkh4hp9a42flHJApo1m', 'admin', 1, NULL),
 ('Невена', 'Гаджева', 'temp@abv.bg', '$2y$10$u.wQ9zr0Es/TRTdgweM/mOi1LM36/IS1RnIxBccTuj/uPChaRwz7q', 'temporary', 6, NULL),
 ('Трифон', 'Трифонов', 'trifonFMI@gmail.com', '$2y$10$uljynZe3M9uI7NCPZCbbDOMZcCn7bVp/wPsHKdu0/PK1qb5tV0X1G', 'permanent', 7, NULL),
 ('Милен', 'Петров', 'm.petrovFMI@abv.bg', '$2y$10$B8v5YexEH6TcVMW0cpL7o.AUk9aHgEgL8cC6nC6sUZifTBruwYuqS', 'permanent', 28, NULL),
