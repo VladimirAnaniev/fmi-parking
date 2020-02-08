bd03e03ecbc20b134ea9542b364f147e6607bceb
Hakan Halil <hhalil@hhalil-a01.vmware.com>
2020-02-08 16:06:26 +0200

    Fix naming. Move auto-generated getters and setters to the bottom.

--- models/User.php
+++ models/User.php
@@ -4,6 +4,63 @@ require 'Course.php';
 
 class User
 {
+    private $u_first;
+    private $u_last;
+    private $u_email;
+    private $u_role;
+    private $u_id;
+    private $car;
+
+    /**
+     * User constructor.
+     * @param $u_first
+     * @param $u_last
+     * @param $u_email
+     * @param $u_role
+     * @param $u_id
+     * @param $car
+     */
+    public function __construct($u_first, $u_last, $u_email, $u_role, $u_id, $car)
+    {
+        $this->u_first = $u_first;
+        $this->u_last = $u_last;
+        $this->u_email = $u_email;
+        $this->u_role = $u_role;
+        $this->u_id = $u_id;
+        $this->car = $car;
+    }
+
+
+    public static function getAllUsers($conn)
+    {
+        $result = $conn->query("SELECT * FROM `users`;");
+        return $result;
+    }
+
+    public static function addUser($conn, $args)
+    {
+        $result = $conn
+            ->prepare("INSERT INTO `users` (`u_first`, `u_last`, `u_email`, `u_role`) VALUES (?,?,?,?);")
+            ->execute($args);
+        return $result;
+    }
+
+    public static function getCourses($conn, $userId) {
+        $sql = "SELECT * FROM courses WHERE teacher_id = :id";
+        $query = $conn->prepare($sql);
+        $query->execute(['id' => $userId]);
+
+        $courses = array();
+        while ($row = $query->fetch()) {
+            $course = new Course($row["course_id"], $row["course_title"], $row["teacher_id"], $row["course_day"],
+            $row["course_from"], $row["course_to"]);
+
+            array_push($courses, $course);
+        }
+
+        return $courses;
+    }
+
     /**
      * @return mixed
      */
@@ -98,61 +155,5 @@ class User
     public function setCar($car)
     {
         $this->car = $car;
-    }
-    private $u_first;
-    private $u_last;
-    private $u_email;
-    private $u_role;
-    private $u_id;
-    private $car;
-
-    /**
-     * User constructor.
-     * @param $u_first
-     * @param $u_last
-     * @param $u_email
-     * @param $u_role
-     * @param $u_id
-     * @param $car
-     */
-    public function __construct($u_first, $u_last, $u_email, $u_role, $u_id, $car)
-    {
-        $this->u_first = $u_first;
-        $this->u_last = $u_last;
-        $this->u_email = $u_email;
-        $this->u_role = $u_role;
-        $this->u_id = $u_id;
-        $this->car = $car;
-    }
-
-
-    public static function getAllUsers($conn)
-    {
-        $result = $conn->query("SELECT * FROM `users`;");
-        return $result;
-    }
-
-    public static function addUser($conn, $args)
-    {
-        $result = $conn
-            ->prepare("INSERT INTO `users` (`u_first`, `u_last`, `u_email`, `u_role`) VALUES (?,?,?,?);")
-            ->execute($args);
-        return $result;
-    }
-
-    public static function getCourses($conn, $userId) {
-        $sql = "SELECT * FROM courses WHERE teacher_id = :id";
-        $query = $conn->prepare($sql);
-        $query->execute(['id' => $userId]);
-
-        $arrayIds = array();
-        while ($row = $query->fetch()) {
-            $course = new Course($row["course_id"], $row["course_title"], $row["teacher_id"], $row["course_day"],
-            $row["course_from"], $row["course_to"]);
-
-            array_push($arrayIds, $course);
-        }
-
-        return $arrayIds;
     }
 }
