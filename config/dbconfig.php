4b37f0a1ac8714397069223c43fc0534977ea88e
root <root@localhost.localdomain>
2020-02-09 20:35:34 +0200

    Move db config to separate file

---
+++ config/dbconfig.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST_NAME = "localhost";
+const DB_USERNAME = "root";
+const DB_PASSWORD = "";
+const DB_NAME = "parkingdb";
--- models/ParkingDB.php
+++ models/ParkingDB.php
@@ -1,9 +1,6 @@
 <?php
 
-const DB_HOST_NAME = "localhost";
-const DB_USERNAME = "root";
-const DB_PASSWORD = "";
-const DB_NAME = "parkingdb";
+require_once 'config/dbconfig.php';
 
 class ParkingDB
 {
