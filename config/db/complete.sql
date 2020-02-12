0a2686d37a610df7b7878392e4388fe93c0edec4
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2020-02-12 20:48:14 +0200

    Change name in db

--- config/db/complete.sql
+++ config/db/complete.sql
@@ -111,7 +111,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`u_first`, `u_last`, `u_email`, `u_password`, `u_role`, `u_id`, `car`) VALUES
 ('Админисандър', 'Щъркелов', 'admin@abv.bg', '$2y$10$36PFZVtgrbPgrznEhcA.M.Kn9clEk966F6fkh4hp9a42flHJApo1m', 'admin', 1, NULL),
-('Невена', 'Гаджева', 'temp@abv.bg', '$2y$10$u.wQ9zr0Es/TRTdgweM/mOi1LM36/IS1RnIxBccTuj/uPChaRwz7q', 'temporary', 6, NULL),
+('Гоша', 'Розалска', 'temp@abv.bg', '$2y$10$u.wQ9zr0Es/TRTdgweM/mOi1LM36/IS1RnIxBccTuj/uPChaRwz7q', 'temporary', 6, NULL),
 ('Трифон', 'Трифонов', 'trifonFMI@gmail.com', '$2y$10$uljynZe3M9uI7NCPZCbbDOMZcCn7bVp/wPsHKdu0/PK1qb5tV0X1G', 'permanent', 7, NULL),
 ('Милен', 'Петров', 'm.petrovFMI@abv.bg', '$2y$10$B8v5YexEH6TcVMW0cpL7o.AUk9aHgEgL8cC6nC6sUZifTBruwYuqS', 'permanent', 28, NULL),
 ('Hakan', 'Halil', 'haki@abv.bg', '$2y$10$zoa88qePnyNpOudVrCKDyeSp14p5ZC6QZ8jdREfpBRZOmEFOm8eM6', 'temporary', 31, NULL);
